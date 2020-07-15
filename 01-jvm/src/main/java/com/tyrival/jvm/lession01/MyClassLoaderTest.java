e0735cc92e760c74efff263b8d0369f2bf555e95
tyrival <tyrival@qq.com>
2020-07-15 15:24:54 +0800

    初始化

--- 01-jvm/src/main/java/com/tyrival/jvm/lession01/MyClassLoaderTest.java
+++ 01-jvm/src/main/java/com/tyrival/jvm/lession01/MyClassLoaderTest.java
@@ -32,18 +32,62 @@ public class MyClassLoaderTest {
                 throw new ClassNotFoundException();
             }
         }
+
+        /**
+         * 重写类加载方法，实现自己的加载逻辑，不委派给双亲加载
+         *
+         * @param name
+         * @param resolve
+         * @return
+         * @throws ClassNotFoundException
+         */
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            synchronized (getClassLoadingLock(name)) {
+                // First, check if the class has already been loaded
+                Class<?> c = findLoadedClass(name);
+
+                if (c == null) {
+                    // If still not found, then invoke findClass in order
+                    // to find the class.
+                    long t1 = System.nanoTime();
+
+                    // 非自定义的类还是走双亲委派加载
+                    if (!name.startsWith("com.tyrival.jvm.lession01")) {
+                        c = this.getParent().loadClass(name);
+                    } else {
+                        c = findClass(name);
+                    }
+
+                    // this is the defining class loader; record the stats
+                    sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+                    sun.misc.PerfCounter.getFindClasses().increment();
+                }
+                if (resolve) {
+                    resolveClass(c);
+                }
+                return c;
+            }
+        }
     }
 
     public static void main(String args[]) throws Exception {
 
         //初始化自定义类加载器，会先初始化父类ClassLoader，其中会把自定义类加载器的父加载 器设置为应用程序类加载器AppClassLoader
-        MyClassLoader classLoader = new MyClassLoader("/Users/tyrival/Workspace/architect-knowledge/01-jvm/target/classes/com/tyrival/jvm/lession01/");
-
-        //D盘创建 test/com/tuling/jvm 几级目录，将User类的复制类User1.class丢入该目录
+        MyClassLoader classLoader = new MyClassLoader(
+                "/Users/tyrival/Workspace/architect-knowledge/01-jvm/target/classes/com/tyrival/jvm/lession01/");
+        // 将User类的复制类User1.class丢入该目录
         Class clazz = classLoader.loadClass("com.tyrival.jvm.lession01.User");
         Object obj = clazz.newInstance();
         Method method = clazz.getDeclaredMethod("sout", null);
         method.invoke(obj, null);
         System.out.println(clazz.getClassLoader().getClass().getName());
+
+        MyClassLoader classLoader1 = new MyClassLoader(
+                "/Users/tyrival/Workspace/architect-knowledge/01-jvm/target/classes/com/tyrival/jvm/lession01/test/");
+        Class clazz1 = classLoader1.loadClass("com.tyrival.jvm.lession01.User");
+        Object obj1 = clazz1.newInstance();
+        Method method1 = clazz1.getDeclaredMethod("sout", null);
+        method1.invoke(obj1, null);
+        System.out.println(clazz1.getClassLoader().getClass().getName());
     }
 }
--- 01-jvm/src/main/java/com/tyrival/jvm/lession01/User.java
+++ 01-jvm/src/main/java/com/tyrival/jvm/lession01/User.java
@@ -1,8 +1,20 @@
 package com.tyrival.jvm.lession01;
 
+import com.tyrival.jvm.lession02.OOMTest;
+
 public class User {
 
+    private Integer id;
+
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -14,5 +26,19 @@ public class User {
 
     public void sout() {
         System.out.println("=======自己的加载器加载类调用方法=======");
+    }
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        OOMTest.list.add(this);
+        System.out.println("关闭资源，userid=" + id + "即将被回收");
     }
 }
