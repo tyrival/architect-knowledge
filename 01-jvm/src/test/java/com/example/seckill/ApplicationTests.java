c2f5a82d3ba2791a719b902afdf54faf46590249
tyrival <tyrival@qq.com>
2020-07-24 20:16:19 +0800

    初始化

---
+++ 01-jvm/src/test/java/com/example/seckill/ApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.seckill;
+
+import com.tyrival.jvm.lession07.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={Application.class})
+public class ApplicationTests {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Test
+	public void test() throws Exception {
+		for (int i = 0; i < 10000; i++) {
+			String result = restTemplate.getForObject("http://localhost:8080/user/process", String.class);
+			Thread.sleep(1000);
+		}
+	}
+
+}
