e7c713863d28fb66134a6dc44d748271770d0c6b
tyrival <tyrival@qq.com>
2020-08-02 15:49:51 +0800

    初始化

--- Jetbrain-FK/src/main/java/Crack.java
+++ Jetbrain-FK/src/main/java/Crack.java
@@ -4,7 +4,7 @@ public class Crack {
 
     // b64488729b831330072b051ce45172c8
     public static void main(String[] args) {
-        String result = "73.25%";
+        String result = "73.25";
         for (int i = 0; i < 100000000; i++) {
             result = DigestUtils.md2Hex(result);
         }
