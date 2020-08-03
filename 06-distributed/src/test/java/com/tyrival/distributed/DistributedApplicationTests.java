1de10bdb70e4e1d6f9df1cfd2b2a1ea9f1d672f0
tyrival <tyrival@qq.com>
2020-08-03 15:46:24 +0800

    初始化

---
+++ 06-distributed/src/test/java/com/tyrival/distributed/DistributedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrival.distributed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
